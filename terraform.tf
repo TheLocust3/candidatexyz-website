d992fb0217b896b5d4ec3227e261e4ab54c6d963
TheLocust3 <jake.kinsella@gmail.com>
2018-06-17 11:57:04 -0400

    Add tags to terraform

--- terraform.tf
+++ terraform.tf
@@ -223,6 +223,12 @@ resource "aws_autoscaling_group" "autoscaling" {
   lifecycle {
     create_before_destroy = true
   }
+
+  tag {
+    key                 = "Name"
+    value               = "${var.name}"
+    propagate_at_launch = true
+  }
 }
 
 resource "aws_db_instance" "db_instance" {
