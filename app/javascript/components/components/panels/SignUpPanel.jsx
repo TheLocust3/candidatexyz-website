70c3035bbc53158e2ed0b49dd3416540f4168a9d
TheLocust3 <jake.kinsella@gmail.com>
2018-07-29 11:04:51 -0400

    Fix mobile view

--- app/javascript/components/components/panels/SignUpPanel.jsx
+++ app/javascript/components/components/panels/SignUpPanel.jsx
@@ -8,10 +8,10 @@ class SignUpPanel extends React.Component {
     render() {
         return (
             <div className='content' id='sign-up'>
-                <div className='mdc-typography--headline3'>Join Us</div>
+                <Text type='headline3'>Join Us</Text>
 
-                <div className='splitscreen1'>
-                    <Text type='body1'>
+                <div className='splitscreen1 relative'>
+                    <Text className='splitscreen1 desktop-middle' type='body1'>
                         Sign up to show your interest in candidateXYZ. We'll contact you asking you to help us test our software on your campaign. We'll provide a slick website and manage your campaign's technical work. You'll focus on running for office.
                     </Text>
                 </div>
